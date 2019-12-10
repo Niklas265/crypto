3961057d2a35e7b1c0207afaff00a6313c12ee0c
DanielEbert <iuq@web.de>
2019-12-10 19:42:56 +0100

    Code done for Praktikum-Primzahlen, TODO: Comments

--- AK/src/Praktikum-Primzahlen/MillerRabinTest.cpp
+++ AK/src/Praktikum-Primzahlen/MillerRabinTest.cpp
@@ -8,9 +8,39 @@
 using namespace std;
 
 Integer MillerRabinTest::boundedExponentation(const Integer& b, const Integer& e, const Integer& bound) const {
+    Integer d("1");
+
+    for (int i = e.BitCount()-1; i >= 0; i--) {
+        d = (d * d);
+        if (d > bound) return bound + 1;
+        if (e.GetBit(i) == 1) {
+            d = (d * b);
+        }
+        if (d > bound) return bound + 1;
+    }
+
+    return d;
 }
 
 bool MillerRabinTest::isPerfectPower(const Integer& n, Integer& b, Integer& e) const {
+    bool found = false;
+    Integer curE = 2;
+    Integer curB;
+    while (curE <= n.BitCount() + 1 && !found) {
+        curB = searchBase(n, curE);
+        if (curB > 0) {
+            found = true;
+            break;
+        } else {
+            curE += 1;
+        }
+    }
+    if (found) {
+        b = curB;
+        e = curE;
+        return true;
+    }
+    return false;
 }
 
 Integer MillerRabinTest::modularExponentation
@@ -20,14 +50,98 @@ Integer MillerRabinTest::modularExponentation
  const Integer& n
 ) const
 {
+    Integer d("1");
+
+    for (int i = b.BitCount()-1; i >= 0; i--) {
+        d = (d * d) % n;
+        if (b.GetBit(i) == 1) {
+            d = (d * a) % n;
+        }
+    }
+
+    return d;
 }
 
 bool MillerRabinTest::witness(const Integer& a, const Integer& n) const {
+    Integer nm = n-1;
+    Integer r;
+    Integer u;
+    // 2^r * u
+
+    for (int i = 0; i < nm.BitCount(); i++) {
+        if (!nm.GetBit(i)) continue;
+        Integer ipow2 = Integer::Power2(i);
+
+        Integer rest = nm / ipow2;
+        if (nm % ipow2 == 0 && rest % 2 == 1) {
+            r = i;
+            u = rest;
+            break;
+        }
+    }
+
+    Integer d = modularExponentation(a, u, n);
+
+    //cout << "r " << r << "u " << u << endl;
+
+    for (int i = 0; i < r; i++) {
+        Integer x = d;
+        d = (d * d) % n;
+        if (d == 1 && x != 1 && x != n-1) {
+            return true;
+        }
+    }
+
+    return d != 1;
 }
 
+
 bool MillerRabinTest::isPrime(PRNG* rng, const Integer& n, unsigned int s) const {
+    if (n % 2 == 0) {
+        return false;
+    }
+
+    for (int i = 0; i < s; i++) {
+        Integer a = rng->getInteger(n-1) + 1;
+        
+        Integer left = a;
+        Integer right = n;
+        Integer tmp;
+        do {
+            tmp = left % right;
+            left = right;
+            right = tmp;
+        } while (right != 0);
+
+        if (left > 1) {
+            return false;
+        }
+        if (witness(a, n)) {
+            return false;
+        }
+    }
+    return true;
 }
 
 
 Integer MillerRabinTest::searchBase(const Integer& n, const Integer& e) const {
+    Integer left = 2;
+    Integer right = n;
+    Integer x;
+    Integer b;
+
+    do {
+        b = left + (right-left) / 2;
+        x = boundedExponentation(b, e, n);
+        if (x > n) {
+            right = b;
+        } else {
+            left = b;
+        }
+    } while (x != n && (right-left) / 2 > 0);
+
+    if (x == n) {
+        return b;
+    }
+    return 0;
 }
--- AK/src/Praktikum-Primzahlen/primes-main.cpp
+++ AK/src/Praktikum-Primzahlen/primes-main.cpp
@@ -11,10 +11,98 @@
 
 using namespace std;
 
+Integer randomPrime(int l, unsigned int s) {
+    if (l % 8 != 0) {
+        cout << "For param l, only multiples of 8 supported." << endl;
+        return Integer("0");
+    }
+
+    MillerRabinTest millerRabinTest;
+    FortunaGenerator fortunaGenerator;
+    byte buf[l/8];
+
+    do {
+        for (int i = 0; i < l/8; i++) {
+            buf[i] = fortunaGenerator.getByte();
+        }
+    } while (!millerRabinTest.isPrime(&fortunaGenerator, Integer(buf, l/8), s));
+
+    return Integer(buf, l/8);
+}
+
 void millerRabin() {
 	cout << "Miller-Rabin Test"
 		 << "================="
 		 << endl << endl;
+
+	MillerRabinTest millerRabinTest;
+	Integer n("121399589920585397786647347761633");
+	for (int i = 3; i < n; i++) {
+	    cout << i << endl;
+	    if (millerRabinTest.witness(Integer(i), n)) {
+	        cout << "n zusammengesetzt mit Witness " << i << endl;
+            break;
+	    }
+	}
+
+    FortunaGenerator fortunaGenerator;
+
+	for (int i = 3; i < 1000; i++) {
+	    if (millerRabinTest.isPrime(&fortunaGenerator, Integer(i), 10)) {
+	        cout << i << endl;
+	    }
+	}
+
+	Integer check1 = Integer("249236727971798483921811058804041930204535828339");
+	cout << check1 << ' ' << millerRabinTest.isPrime(&fortunaGenerator, Integer(check1), 50) << endl;
+
+    Integer check2 = Integer("167376539261987877304745111417636335723391598799");
+    cout << check2 << ' ' << millerRabinTest.isPrime(&fortunaGenerator, Integer(check2), 50) << endl;
+
+    Integer check3 = Integer("207241657224647904979925877995751997643166331621");
+    cout << check3 << ' ' << millerRabinTest.isPrime(&fortunaGenerator, Integer(check3), 50) << endl;
+
+    Integer check4 = Integer("203681457354349143050092586190992078578082388049");
+    cout << check4 << ' ' << millerRabinTest.isPrime(&fortunaGenerator, Integer(check4), 50) << endl;
+
+    assert(millerRabinTest.boundedExponentation(Integer("7"), Integer("43"), Integer::Power2(121))
+        == Integer("2183814375991796599109312252753832343"));
+
+    assert(millerRabinTest.boundedExponentation(Integer("7"), Integer("43"), Integer::Power2(120))
+           == Integer("1329227995784915872903807060280344577"));
+
+    assert(millerRabinTest.searchBase(
+            Integer("346158389003024161132823470747012309343112001349179903402673"),
+            Integer("29")
+        ) == Integer("113"));
+
+    Integer e;
+    Integer b;
+    millerRabinTest.isPerfectPower(
+        Integer("346158389003024161132823470747012309343112001349179903402673"),
+        b, e);
+    cout << "b: " << b << " e: " << e << endl;
+    assert(b == Integer("113"));
+    assert(e == Integer("29"));
+
+    b = 0;
+    e = 0;
+    millerRabinTest.isPerfectPower(
+            Integer("2183814375991796599109312252753832343"),
+            b, e);
+    cout << "b: " << b << " e: " << e << endl;
+
+    b = 0;
+    e = 0;
+    millerRabinTest.isPerfectPower(
+            Integer("1329227995784915872903807060280344577"),
+            b, e);
+    cout << "b: " << b << " e: " << e << endl;
+
+    int primeSizes[] = {512, 1024, 2048, 4096};
+    for (int primeSize : primeSizes) {
+        cout << primeSize << " Bit Prime: " << randomPrime(primeSize, 20) << endl;
+    }
 }
 
 void fortuna() {
@@ -70,7 +158,7 @@ int main() {
 		 << endl << endl;
 
 	fortuna();
-	//millerRabin();
+	millerRabin();
 
 	return 0;
 }
