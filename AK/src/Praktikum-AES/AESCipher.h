decb77caf7bf6e23bfba303277a1079de228c5e7
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2020-01-14 11:22:20 +0100

    Doku ohne process

--- AK/src/Praktikum-AES/AESCipher.h
+++ AK/src/Praktikum-AES/AESCipher.h
@@ -42,16 +42,70 @@ public:
 
     AESCipher(bool debug_mode=false);
 
+    /**
+    * Die Methode setKey() ruft die Methode setKey des key_schedules auf, der den
+    * key_schedule für den übergebenen Schlüssel berechnet
+    *
+    * @param key Schlüssel als Byte-Vektor, für den der Key-Schedule berechnet werden soll
+    *
+    * @ return True, wenn die Berechnung des Key-Schedules erfolgreich ablief. False, wenn
+    * die Berechnung des Key-Schedules nicht erfolgreich war
+    */
     bool setKey(const vector<byte>& key);
 
+    /**
+    * Die Methode encryptBlock() verschlüsselt einen 16-Byte großen Plaintextblock
+    * mit dem AES. Dafür werden für jede Runde der Reihe nach die Basisoperationen
+    * subBytes, shiftRows, mixColumns und addKey in dieser Reihenfolge aufgerufen.
+    * Bei der Operation addKey wird darüber hinaus der Rundenschlüssel für die akuelle
+    * Runde aus dem key_schedule benötigt. In der letzten Runde wird auf die
+    * Basisoperation mixColumns verzichtet.
+    *
+    * @param plain_text Zeiger auf ein Byte-Array der Größe 16, welches mit dem AES
+    * verschlüsselt werden soll.
+    * @param cipher_text Zeiger auf ein Byte-Array der Größe 16 in dem das Ergebnis der
+    * Verschlüsselung abgelegt wird.
+    */
     void encryptBlock(const byte *plain_text, byte *cipher_text);
 
+    /**
+    * In decryptBlock() wird ein 16 Byte großer Geheimtextblock entschlüsselt. Dafür werden
+    * die inversen Basisoperationen in umgekehrter Reihenfolge zur Verschlüsselung eines
+    * Blocks aufgerufen. Für eine Runde ist das: addKey, invMixColumns, invShiftRows, invSubbytes.
+    * Für die erste Runde, in welcher die letzte Runde der Verschlüsselung invertiert wird,
+    * wird auf die invMixColumns-Operation verzichtet.
+    *
+    * @param cipher_text Zu entschlüsselnder Geheimtextblock der Länge 16 Bytes
+    * @param plain_text In plain_text wird der entschlüsselte Block abgelegt
+    */
     void decryptBlock(const byte *cipher_text, byte *plain_text);
 
     bool process(const vector<byte>& in, vector<byte>& out, bool mode);
 
+    /**
+    * Diese Methode verschlüsselt einen Klartext plain_text mit dem AES und
+    * speichert das Ergebnis in cipher_text. Dabei wird jeder 16-Byte große 
+    * Block über die Methode encryptBlock() einzeln verschlüsselt. Die Länge
+    * des zu verschlüsselnden Klartexts muss ein vielfaches von 16 sein.
+    *
+    * @param plain_text Byte-Vektor, der den zu verschlüsselnden Klartext enthält
+    * @param cipher_text Byte-Vektor, in dem der verschlüsselte Geheimtext abgelegt
+    * wird
+    *
+    * @return True, falls die Verschlüsselung erfolgreich war, ansonsten False
+    */
     bool encrypt(const vector<byte>& plain_text, vector<byte>& cipher_text);
 
+    /**
+    * Die Methode decrypt() entschlüsselt den übergebenen Geheimtext cipher_text und
+    * legt den entschlüsselnden Klartext in plain_text ab. Die Länge des zu entschlüsselnden
+    * Geheimtexts muss ein Vielfaches von 16 sein.
+    *
+    * @param cipher_text Byte-Vektor, der den zu entschlüsselnden Geheimtext enthält
+    * @param plain_text in dem der entschlüsselte Geheimtext abgelegt wird
+    *
+    * @return True, falls die Entschlüsselung erfolgreich war, ansonsten False
+    */
     bool decrypt(const vector<byte>& cipher_text, vector<byte>& plain_text);
 
     static vector<byte> toVector(const string& msg, size_t block_len=16);
