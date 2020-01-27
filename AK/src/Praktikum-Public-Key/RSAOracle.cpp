ab45db08a6c6835aec55ca17960ce2161410cfbc
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2020-01-27 14:49:40 +0100

    Kommentare

--- AK/src/Praktikum-Public-Key/RSAOracle.cpp
+++ AK/src/Praktikum-Public-Key/RSAOracle.cpp
@@ -10,7 +10,10 @@
 using namespace std;
 
 RSAOracle::RSAOracle(Integer& p, Integer& q, Integer& d) {
+    //Konstruktor von RSAOracle
+    //setze n auf das Ergebnis der Multiplikation der beiden Primzahlen p und q
     n = p * q;
+    //Weise dem Integer-Attribut d des Objekts das übergebenen d zu
     this->d = d;
 }
 
@@ -19,11 +22,22 @@ RSAOracle::~RSAOracle() {
 
 // #half()
 bool RSAOracle::half(const Integer& y) const {
+    //Methode half(), die für einen Geheimtext das höchstwertigste Bit des
+    //zugehörigen Klartexts zurücklieferts
 
+    //Erzeugen eines Objekts der Klasee PublicKeyAlgorithmBox, in dem die
+    //anschließende Modulare Exponentation zum entschlüsseln des Geheimtexts
+    //implementiert ist
     PublicKeyAlgorithmBox pb;
 
+    //Entschlüsseln des übergebenen Geheimtexts y und abspeichern in der 
+    //Variable x. Dazu wird die in der Klasse PublicKeyAlgorithmBox implementiert
+    //Methode zur Modularen Exponentation eingesetzt und y^d (mod n) berechnet,
+    //was der Entschlüsselung im RSA-Kryptosystem entspricht
     Integer x = pb.modularExponentation(y, d, n);
 
+    //Rückgabe von true, wenn berechnete x größer als n/2 ist bzw. False ansonsten
+    //Dies entspricht dem höchstwertigsten Bit des Klartexts
     return x > n / 2;
 }
 
