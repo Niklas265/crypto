62fe002c72ba06ef33608266770d5b2732eda477
Niklas265 <59866153+Niklas265@users.noreply.github.com>
2020-01-27 14:38:58 +0100

    Doku RSAOracle
    TODO: Parity

--- AK/src/Praktikum-Public-Key/RSAOracle.h
+++ AK/src/Praktikum-Public-Key/RSAOracle.h
@@ -11,7 +11,10 @@ using namespace std;
 using namespace CryptoPP;
 
 /**
- * TODO
+ * Diese Klasse implementiert das RSA-Orakel, dass für die Half-Attack benötigt wird.
+ * Das Orakel besitzt kenntnis über den geheimen RSA-Schlüssel und ist so in der Lage,
+ * Aussagen über das höchstwertigste Bit eines Klartexts mit dem dazugehörigen
+ * Geheimtexts zu treffen.
  */
 class RSAOracle {
 private:
@@ -19,10 +22,35 @@ private:
 	Integer d;
 
 public:
+	/***
+	* Der Konstruktor der Klasse weißt die übergebenen Parametern den entsprechenden
+	* Attributen des erzeugten Objekts zu. Dabei wird n = p*q und this->d = d zugewiesen.
+	*
+	* @param p Primzahl p, die zum Berechnen von n des RSA-Schlüssels eingesetzt wird
+	* @param q Primzahl q, welche zur Berechnung von n des RSA-Schlüssels eingesetzt wird
+	* @param d Parameter d des geheimen Teils des RSA-Schlüssels, über den der Klartext
+	* berechnet wird
+	*/
 	RSAOracle(Integer& p, Integer& q, Integer& d);
 	virtual ~RSAOracle();
 
+	/***
+	* Die Methode half() wird zur Durchführung einer Half-Attack auf RSA verwendet. Diese Methode
+	* bekommt einen Geheimtext übergeben und liefert den Wert des höchstwertigen Bits des zu dem
+	* Geheimtext gehörenden Klartexts zurück. Da keine solche Funktion existiert, besitzt das Orakel
+	* in dieser Implementierung Wissen über den privaten Teil des RSA-Schlüssels, um den Geheimtext
+	* entschlüsseln zu können und auf diese Weise das höchstwertigste Bit zu bestimmen.
+	*
+	* @param y Geheimtext, für den der Wert des höchstwertigsten Bits des zugehörigen Klartextes
+	* bestimmt werden soll
+	*
+	* @return True, falls das höchstwertigste Bit des zugehörigen Klartexts 1 ist, ansonsten False.
+	*/
 	bool half(const Integer& y) const;
+	
+	/***
+	* TODO
+	*/
 	bool parity(const Integer& y) const;
 
 };
